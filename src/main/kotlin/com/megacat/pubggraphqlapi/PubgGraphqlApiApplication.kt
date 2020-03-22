7153a3e66dac142788b987e93c47f86695cdf935
ea-chatr <ea-chatr@mattilsynet.no>
2020-03-22 19:21:58 +0100

    Initial commit

---
+++ src/main/kotlin/com/megacat/pubggraphqlapi/PubgGraphqlApiApplication.kt
@@ -0,0 +1,11 @@
+package com.megacat.pubggraphqlapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PubgGraphqlApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<PubgGraphqlApiApplication>(*args)
+}
